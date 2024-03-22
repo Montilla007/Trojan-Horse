cbe15b8212adb327288e8d5ad45f91a3a73198cc
Dyen <deanielaquino7@gmail.com>
2024-03-22 10:03:28 +0800

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\StudentsController;
+use App\Http\Controllers\Api\TeachersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/student/register', [StudentsController::class, 'register']);
+Route::post('/auth/student/login', [StudentsController::class, 'login']);
+Route::post('/auth/student/logout', [StudentsController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/auth/teacher/register', [TeachersController::class, 'register']);
+Route::post('/auth/teacher/login', [TeachersController::class, 'login']);
+Route::post('/auth/teacher/logout', [TeachersController::class, 'logout'])->middleware('auth:sanctum');
