e16e6d6bc5547ed8e01057e96f36d91bb673687c
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2024-03-23 15:08:01 +0800

    title and description now nullable

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -78,8 +78,8 @@ public function create(Request $request, int $classroom_id)
 {
     // Validate the request data
     $validatedData = $request->validate([
-        'title' => 'required|string',
-        'description' => 'required|string',
+        'title' => 'nullable|string',
+        'description' => 'nullable|string',
         'start_time' => 'required|date_format:Y-m-d H:i:s',
         'end_time' => 'required|date_format:Y-m-d H:i:s',
         'teacher_id' => 'required|exists:teachers,id',
